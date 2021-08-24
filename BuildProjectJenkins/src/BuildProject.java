6a2c79f5d12db3b4049033d77b689539d29dd40f
QANaveen <89070541+QANaveen@users.noreply.github.com>
2021-08-24 12:19:07 +0530

    Update BuildProject.java

--- BuildProjectJenkins/src/BuildProject.java
+++ BuildProjectJenkins/src/BuildProject.java
@@ -6,6 +6,7 @@ public class BuildProject {
 
 		System.out.println("Build project in jenkins");
 		System.out.println("Github code gets Build automatically");
+		Systemm.out.println("Hello world");
 		
 		
 	}
